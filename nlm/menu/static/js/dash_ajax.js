e1a526019d5210a5cded33f49112be28becb4e53
David <exinmusic@gmail.com>
2018-11-27 17:06:10 -0800

    starting new dash

--- nlm/menu/static/js/dash_ajax.js
+++ nlm/menu/static/js/dash_ajax.js
@@ -4,6 +4,18 @@
 		$.each(obj.nopheno, function(key, value) {
 			$("#nopheno").append('<tr><td>'+value[0]+'</td></tr>');
 		});
+		$("#sativas").empty();
+		$.each(obj.sativas, function(key, value) {
+			$("#sativas").append('<tr><td>'+value[0]+'</td></tr>');
+		});
+		$("#hybrids").empty();
+		$.each(obj.hybrids, function(key, value) {
+			$("#hybrids").append('<tr><td>'+value[0]+'</td></tr>');
+		});
+		$("#indicas").empty();
+		$.each(obj.indicas, function(key, value) {
+			$("#indicas").append('<tr><td>'+value[0]+'</td></tr>');
+		});
 	});
 	setTimeout(arguments.callee, 15000);
 })();
